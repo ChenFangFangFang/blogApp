17b442adb26ff676d7c5d4de674b04e10a1f9d2d
ChenFangFangFang <sophiachan0430@gmail.com>
2024-12-08 13:15:10 +0200

    change style to Tailwind

--- bloglist-frontend/src/components/Layout.jsx
+++ bloglist-frontend/src/components/Layout.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Typography from "@mui/material/Typography";
 import { setUser, clearUser } from "../reducers/userReducer";
 import userStorage from "../services/userStorage";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,8 +8,25 @@ import Notification from "./Notification";
 import { setNotification } from "../reducers/notificationReducer";
 import loginService from "../services/login";
 import Login from "./Login";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems
+} from "@headlessui/react";
 
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 const Layout = ({ children }) => {
+  const navigation = [
+    { name: "Home", href: "/", key: "home", current: true },
+    { name: "User", href: "/users", key: "user", current: false }
+  ];
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
   const dispatch = useDispatch();
 
   const user = useSelector((state) => state.user);
@@ -51,9 +67,6 @@ const Layout = ({ children }) => {
   if (!user) {
     return (
       <div>
-        <Typography variant="h4" gutterBottom>
-          Blog Application
-        </Typography>
         <Notification />
         <Login login={handleLogin} />
       </div>
@@ -80,7 +93,7 @@ const Layout = ({ children }) => {
 
   return (
     <div>
-      <div style={navStyle}>
+      {/* <div style={navStyle}>
         <Link style={linkStyle} to="/">
           Home
         </Link>
@@ -90,8 +103,101 @@ const Layout = ({ children }) => {
         <div style={userInfoStyle}></div>
         {user && <div>{user.name} logged in</div>}
         <button onClick={handleLogout}>Logout</button>
-      </div>
-      <div>{children}</div> {/* Render page-specific content here */}
+      </div> */}
+      <Disclosure as="nav" className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon
+                  aria-hidden="true"
+                  className="block size-6 group-data-[open]:hidden"
+                />
+                <XMarkIcon
+                  aria-hidden="true"
+                  className="hidden size-6 group-data-[open]:block"
+                />
+              </DisclosureButton>
+            </div>
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+                  className="h-8 w-auto"
+                />
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.key}
+                      to={item.href}
+                      aria-current={item.current ? "page" : undefined}
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-900 text-white"
+                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "rounded-md px-3 py-2 text-sm font-medium"
+                      )}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+            {/* <span className="text-white">
+              {user && <div>{user.name} logged in</div>}
+            </span>
+            <div>
+              <button className="bg-blue-900 text-white" onClick={handleLogout}>
+                Logout
+              </button>
+            </div> */}
+            <span className="text-white font-semibold mr-4">
+              {user && (
+                <div className="flex items-center space-x-2">
+                  <span className="inline-block bg-gray-700 rounded-full px-3 py-1 text-sm">
+                    {user.name}
+                  </span>
+                  <span>logged in</span>
+                </div>
+              )}
+            </span>
+            <div>
+              <button
+                className="bg-red-600 hover:bg-red-700 text-white font-medium py-1 px-4 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 transition"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+        <DisclosurePanel className="sm:hidden">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navigation.map((item) => (
+              <Link
+                key={item.key}
+                to={item.href}
+                aria-current={item.current ? "page" : undefined}
+                className={classNames(
+                  item.current
+                    ? "bg-gray-900 text-white"
+                    : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                  "block rounded-md px-3 py-2 text-base font-medium"
+                )}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </DisclosurePanel>
+      </Disclosure>
+      <div>{children}</div>
     </div>
   );
 };
--- bloglist-frontend/src/components/Login.jsx
+++ bloglist-frontend/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "../reducers/userReducer";
-import TextField from "@mui/material/TextField";
 
 const Login = ({ login }) => {
   const [username, setUsername] = useState("");
@@ -18,25 +17,68 @@ const Login = ({ login }) => {
     }
   };
   return (
-    <form onSubmit={handleLogin}>
-      <TextField
-        id="filled-basic"
-        label="username"
-        variant="filled"
-        value={username}
-        onChange={({ target }) => setUsername(target.value)}
-      />
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
 
-      <TextField
-        type="password"
-        id="filled-basic"
-        label="username"
-        variant="filled"
-        value={password}
-        onChange={({ target }) => setPassword(target.value)}
-      />
-      <input type="submit" value="Login" />
-    </form>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleLogin} className="space-y-6">
+            <div>
+              <label
+                htmlFor="text"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="username"
+                  required
+                  autoComplete="username"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  onChange={({ target }) => setUsername(target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="text"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="password"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  onChange={({ target }) => setPassword(target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                value="Login"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
